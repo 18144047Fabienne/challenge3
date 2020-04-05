fd431453f0f9f0bad7fab9305e33236b9a8eef7a
18144047Fabienne <61313107+18144047Fabienne@users.noreply.github.com>
2020-04-05 14:28:51 +0200

    nieuwe map

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+// Set api token
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmFiaWVubmUxODE0NDA0NyIsImEiOiJjazhrNHpodWwwMjFkM2duNTJkNjd4OHV2In0.aQ93jmoLWUrV8aBM0q4cew';
+
+// Initialate map
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [4.322840, 52.067101],
+  zoom: 11.15
+});
+
+// Voeg de zoekbalk toe
+map.addControl(
+  new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+  })
+);
+
+// Voeg de planner toe
+map.addControl(
+  new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+  }),
+  'top-left'
+);
